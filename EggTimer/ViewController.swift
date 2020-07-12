ef2a304557520b129f23ea678ab78b406c60db70
Derek Janis <dmjanis@gmail.com>
2020-07-12 16:58:32 -0400

    Through Lesson 95

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-        
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    var counter = 60
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var eggTitle: UILabel!
     
     //button to select egg hardness
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
+        
+        counter = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds remaining.")
+            counter -= 1
+        }
+        else {
+            timer.invalidate()
+            eggTitle.text = "Done!"
+        }
     }
 }
+
 
